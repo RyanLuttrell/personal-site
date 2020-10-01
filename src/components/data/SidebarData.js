681ba574f8d7e663618b512e31c1332ae2441e8a
RyanLuttrell <ryan.m.luttrell@gmail.com>
2020-10-01 14:58:13 -0600

    Update transition speed on nav elements

--- src/components/data/SidebarData.js
+++ src/components/data/SidebarData.js
@@ -3,31 +3,31 @@ export const SidebarData = [
         title: 'Home',
         path: '/',
         cName: 'nav-text'
-    },
-    {
-        title: 'About',
-        path: '/about',
-        cName: 'nav-text'
-    },
-    {
-        title: 'Projects',
-        path: '/projects',
-        cName: 'nav-text'
-    },
-    {
-        title: 'Blog',
-        path: '/blog',
-        cName: 'nav-text'
-    },
-    {
-        title: 'Resume',
-        path: '/resume',
-        cName: 'nav-text'
-    },
-    {
-        title: 'Contact',
-        path: '/contact',
-        cName: 'nav-text'
     }
+    // {
+    //     title: 'About',
+    //     path: '/about',
+    //     cName: 'nav-text'
+    // },
+    // {
+    //     title: 'Projects',
+    //     path: '/projects',
+    //     cName: 'nav-text'
+    // },
+    // {
+    //     title: 'Blog',
+    //     path: '/blog',
+    //     cName: 'nav-text'
+    // },
+    // {
+    //     title: 'Resume',
+    //     path: '/resume',
+    //     cName: 'nav-text'
+    // },
+    // {
+    //     title: 'Contact',
+    //     path: '/contact',
+    //     cName: 'nav-text'
+    // }
 
 ]
